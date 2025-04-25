90955293d75283a86202006b0224870a687f2835
viviramji <viviramji@gmail.com>
2025-04-24 19:28:15 -0500

    Dummy commit

--- terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "az_client_id" { default = "XXX..." }  
-variable "az_client_secret" { default = "XXX..." }
-variable "az_tenant" { default = "XXX..." }
-variable "az_subscription" { default = "XXX..." }
-
-variable "az_location" { default = "eastus" }
-variable "az_resource_group_name" { default = "homework" }
